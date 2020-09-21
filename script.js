4fa02b1c2c16c31b57f004ac65fa5e5f12148148
jairo <jairo.angulo93@gmail.com>
2020-09-21 10:55:15 -0400

    Added 3 files

---
+++ script.js
@@ -0,0 +1,11 @@
+// async function getAllResults(userInput) {
+//   try {
+//     const url = `https://itunes.apple.com/search?term=${userInput}&limit=15`
+//     const response = await axios.get(url)
+//     console.log(response.data)
+//   } catch (error) {
+//     console.log(`Error: ${error}`)
+//   }
+// }
+
+// getAllResults()
