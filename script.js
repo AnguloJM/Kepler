fbedee7fbb280ae8f1c614042b18fc95c2145796
jairo <jairo.angulo93@gmail.com>
2020-09-21 17:29:45 -0400

    Functioning search form

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-// async function getAllResults(userInput) {
-//   try {
-//     const url = `https://itunes.apple.com/search?term=${userInput}&limit=15`
-//     const response = await axios.get(url)
-//     console.log(response.data)
-//   } catch (error) {
-//     console.log(`Error: ${error}`)
-//   }
-// }
+async function fetchData(userInput) {
+  try {
+    const url = `https://itunes.apple.com/search?media=music&term=${userInput}&limit=6`;
+    const response = await axios.get(url);
+    console.log(response.data);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
 
-// getAllResults()
+const form = document.querySelector("#search-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = document.querySelector("#search").value;
+  document.querySelector("#search").value = "";
+  fetchData(inputValue);
+});
+
+// async function topRated() {
+//   "https://itunes.apple.com/search?media=music&attribute=ratingIndex&limit=6"
+//   const container = document.s
+// }
