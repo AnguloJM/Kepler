0f4cbd4452b534a33561c3f9a906f4d2f323a284
jairo <jairo.angulo93@gmail.com>
2020-09-22 09:22:47 -0400

    top rated forloop nearly comeplete

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+async function fetchTopResult() {
+  try {
+    const url = `https://itunes.apple.com/search?media=music&attribute=ratingIndex&limit=6`;
+    const response = await axios.get(url);
+    const data = response.data.results;
+    console.log(response.data.results);
+    showTopResult(data);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
+
+fetchTopResult();
+let resultDiv = document.querySelector("#top-searches");
+function showTopResult(datas) {
+  let results = datas.map((data) => {
+    const image = document.createElement("img");
+    image.setAttribute("src", data.artworkUrl100);
+    resultDiv.appendChild(image);
+
+    const songName = document.createElement("h3");
+    songName.innerHTML = `${data.collectionName}`;
+    resultDiv.appendChild(songName);
+
+    const artist = document.createElement("p");
+    artist.innerHTML = `${data.artistName}`;
+    resultDiv.appendChild(artist);
+
+    // const playSample = document.createElement("AUDIO");
+    // playSample.setAttribute("controls", data.play(previewUrl));
+    // resultDiv.appendChild(playSample);
+    // // playSample.play();
+  });
+
+  return results;
+}
+
 async function fetchData(userInput) {
   try {
     const url = `https://itunes.apple.com/search?media=music&term=${userInput}&limit=6`;
@@ -8,15 +45,10 @@ async function fetchData(userInput) {
   }
 }
 
-const form = document.querySelector("#search-form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const inputValue = document.querySelector("#search").value;
-  document.querySelector("#search").value = "";
-  fetchData(inputValue);
-});
-
-// async function topRated() {
-//   "https://itunes.apple.com/search?media=music&attribute=ratingIndex&limit=6"
-//   const container = document.s
-// }
+// const form = document.querySelector("#search-form");
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const inputValue = document.querySelector("#search").value;
+//   document.querySelector("#search").value = "";
+//   fetchData(inputValue);
+// });
