76ec5ba2f601714e34fa517df9dd6292a417fb01
jairo <jairo.angulo93@gmail.com>
2020-09-23 00:17:15 -0400

    Fully function and updated CSS

--- script.js
+++ script.js
@@ -14,24 +14,69 @@ fetchTopResult();
 let resultDiv = document.querySelector("#top-searches");
 function showTopResult(datas) {
   let results = datas.map((data) => {
+    const newDiv = document.createElement("div");
+    resultDiv.appendChild(newDiv);
+    newDiv.classList = "top-rated-items";
+
     const image = document.createElement("img");
     image.setAttribute("src", data.artworkUrl100);
-    resultDiv.appendChild(image);
+    newDiv.appendChild(image);
 
     const songName = document.createElement("h3");
     songName.innerHTML = `${data.collectionName}`;
-    resultDiv.appendChild(songName);
+    newDiv.appendChild(songName);
 
     const artist = document.createElement("p");
     artist.innerHTML = `${data.artistName}`;
-    resultDiv.appendChild(artist);
+    newDiv.appendChild(artist);
 
     const playSample = document.createElement("audio");
     const source = document.createElement("source");
     source.setAttribute("src", data.previewUrl);
     playSample.setAttribute("controls", data.previewUrl);
     playSample.appendChild(source);
-    resultDiv.appendChild(playSample);
+    newDiv.appendChild(playSample);
+  });
+
+  return results;
+}
+
+async function fetchSearchData(userInput) {
+  try {
+    const url = `https://itunes.apple.com/search?media=music&term=${userInput}&limit=6`;
+    const response = await axios.get(url);
+    const data = response.data.results;
+    showSearchResults(data);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
+
+let resultList = document.querySelector("#top-searches");
+function showSearchResults(datas) {
+  let results = datas.map((data) => {
+    const newDiv = document.createElement("div");
+    resultList.appendChild(newDiv);
+    newDiv.classList = "searched-data";
+
+    const image = document.createElement("img");
+    image.setAttribute("src", data.artworkUrl100);
+    newDiv.appendChild(image);
+
+    const songName = document.createElement("h3");
+    songName.innerHTML = `${data.collectionName}`;
+    newDiv.appendChild(songName);
+
+    const artist = document.createElement("p");
+    artist.innerHTML = `${data.artistName}`;
+    newDiv.appendChild(artist);
+
+    const playSample = document.createElement("audio");
+    const source = document.createElement("source");
+    source.setAttribute("src", data.previewUrl);
+    playSample.setAttribute("controls", data.previewUrl);
+    playSample.appendChild(source);
+    newDiv.appendChild(playSample);
   });
 
   return results;
@@ -40,21 +85,11 @@ function showTopResult(datas) {
 const form = document.querySelector("#search-form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  removeResults();
   const inputValue = document.querySelector("#search").value;
   document.querySelector("#search").value = "";
-  fetchData(inputValue);
+  fetchSearchData(inputValue);
 });
-
-async function fetchSearchData(userInput) {
-  try {
-    const url = `https://itunes.apple.com/search?media=music&term=${userInput}&limit=6`;
-    const response = await axios.get(url);
-    console.log(response.data.results);
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
-}
-fetchSearchData();
 
 function removeResults() {
   const appendElement = document.querySelector("#top-searches");
